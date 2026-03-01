5d5d7a5a6dd6856f77a6dba059c5b06b2b7cf641
Shubham Rana <shubhamrana@Shubhams-MacBook-Air.local>
2026-03-01 17:41:00 +0530

    Remove all tracked files

--- BackEnd/src/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const Songrouter = require('./routes/songs.routes');
-const app = express();
-app.use(express.json());
-app.use('/', Songrouter);
-module.exports = app;
