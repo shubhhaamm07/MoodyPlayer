11dd23512fcdad8ac4a477824c40d5a2ad6adc91
Shubham Rana <shubhamrana@Shubhams-MacBook-Air.local>
2026-03-01 17:35:24 +0530

    Remove all tracked files

--- BackEnd/src/models/songs.models.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const songSchema = new mongoose.Schema({
-    title: String,
-    artist: String,
-    audio: String,
-});
-const Song = mongoose.model("Song", songSchema);
-module.exports = Song;
